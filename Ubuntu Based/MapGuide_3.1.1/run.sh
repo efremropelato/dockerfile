857102e5bb3f4b828b03d39d5f3502fc8292767c
Efrem Ropelato <efrem.ropelato@gmail.com>
2018-11-13 18:56:27 +0100

    fix run mapguide:3.1.1

--- Ubuntu Based/MapGuide_3.1.1/run.sh
+++ Ubuntu Based/MapGuide_3.1.1/run.sh
@@ -1,2 +1,1 @@
-/etc/init.d/apache-mapguide start & 
-sh /usr/local/mapguideopensource-3.1.1/webserverextensions/tomcat/bin/startup.sh s
+/etc/init.d/apache-mapguide start && /etc/init.d/mapguide start && sh /usr/local/mapguideopensource-3.1.1/webserverextensions/tomcat/bin/startup.sh s
