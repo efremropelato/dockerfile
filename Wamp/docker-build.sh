68bbc0c1229cc295db0157fe1b28497042eb25c5
Efrem Ropelato <efrem.ropelato@gmail.com>
2025-04-04 15:07:15 +0200

    build multiarch

--- Wamp/docker-build.sh
+++ Wamp/docker-build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-docker build -t efremropelato/wamp:1.0.0 .
-
+docker build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t efremropelato/wamp:1.0.0 .
 
 # docker run efremropelato/wamp:1.0.0 --ws 0.0.0.0:9000 --realm txt
