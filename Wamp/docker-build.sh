98b2f3598abf26ed94f8a6a99cfd357e94a70c05
Efrem Ropelato <efrem.ropelato@gmail.com>
2025-04-04 15:36:02 +0200

    docker wamp

--- Wamp/docker-build.sh
+++ Wamp/docker-build.sh
@@ -2,4 +2,5 @@
 
 docker build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t efremropelato/wamp:1.0.0 .
 
-# docker run efremropelato/wamp:1.0.0 --ws 0.0.0.0:9000 --realm txt
+# docker run --rm -p 9000:9000 efremropelato/wamp:1.0.0 --ws 0.0.0.0:9000 --realm txt
+# docker run -d --name wamp-test --restart always -e TZ=Europe/Rome -p 9000:9000 efremropelato/wamp:1.0.0 --ws 0.0.0.0:9000 --realm txt
